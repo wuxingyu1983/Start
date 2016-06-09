c5c65d3bb64d1ba936354195e4c4b02d7dae3d41
wuxingyu1983@gmail.com <wuxingyu@wuxingyudeMacBook-Pro.local>
2016-06-09 17:33:33 +0800

    init

---
+++ start/start/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  start
+//
+//  Created by 吴星煜 on 16/6/9.
+//  Copyright © 2016年 imixun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ start/start/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  start
+//
+//  Created by 吴星煜 on 16/6/9.
+//  Copyright © 2016年 imixun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
